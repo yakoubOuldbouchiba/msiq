099a163216cec7ab2db19396538f3daae8f60109
yakoubOuldbouchiba <53263448+yakoubOuldbouchiba@users.noreply.github.com>
2020-10-04 14:31:33 +0100

    prod-server is wrong to be git

--- prod-server/objects/messages/message.js
+++
@@ -1,9 +0,0 @@
-var _classCallCheck = require("C:/Users/Yak/Desktop/MSIQ/msiq-v-1.0.0/node_modules/@babel/runtime/helpers/classCallCheck");
-
-var Message = function Message() {
-  "use strict";
-
-  _classCallCheck(this, Message);
-};
-
-module.exports = Message;
