b65b2b3a45b522ab4bea94470bd16b6742d9f9c2
BinaryHawk <yacinelalmi19@gmail.com>
2020-10-08 01:15:46 +0200

    Fixed the login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,7 @@
+
+Highly recommend
+sent
+44 minutes ago
 <template>
     <div class="mt-8">
     <v-container class="fill-height" fluid>
@@ -9,15 +13,54 @@
                 <h2>Bienvenue</h2>
               </v-flex>
             </v-card-title>
-            <v-form  class="pa-5">
-              <v-select :items="items" label="fonction" prepend-icon="mdi-briefcase" outlined v-model="role" ></v-select>
-              <v-text-field v-model="email"  label="Email" type="text" prepend-icon="mdi-account" required outlined></v-text-field>
-              <v-text-field v-model="password"  label="Mot de passe" type="password" prepend-icon="mdi-lock" required outlined></v-text-field>
-              <v-flex row class="justify-center">
-                <v-btn class="primary" @click="login">Connexion</v-btn>
-                <v-spacer></v-spacer>
+            <v-form  class="pa-5" ref="form" v-model="valid">
+                <v-alert
+                  v-if="showError"
+                  class="ml-8 mb-6"
+                  dense
+                  outlined
+                  type="error"
+                >
+               {{titleError}}
+            </v-alert>
+              <v-select 
+                v-model="user.role"
+                :items="items"
+                :rules="[v => !!v || 'Cet champs est obligatoire']" 
+                label="Type d'utilisateur" 
+                prepend-icon="mdi-briefcase" 
+                outlined>
+              </v-select>
+              <v-text-field 
+                v-model="user.email"
+                label="Email" 
+                :rules="[v => !!v || 'Cet champs est obligatoire']"
+                type="text" 
+                prepend-icon="mdi-account" 
+                required outlined>
+              </v-text-field>
+              <v-text-field 
+                v-model="user.password"
+                label="Mot de passe" 
+                :rules="[v => !!v || 'Cet champs est obligatoire']"
+                type="password" 
+                prepend-icon="mdi-lock" 
+                required 
+                outlined>
+              </v-text-field>
+              <v-flex row class="ml-3 justify-center">
+                <v-btn 
+                :disabled="!valid"
+                  class="primary"
+                  @click="login">
+                Connexion</v-btn>
+              </v-flex>
+              <br/>
+              <v-flex>
+                <a class="float-left"  >Mot de passe oublié ?</a>
+                 <v-spacer></v-spacer>
                 <Register/>
-
+                <br/>
               </v-flex>
             </v-form>
           </v-card>
@@ -26,13 +69,13 @@
     </v-container>
     </div>
 </template>
-
 <script>
 import Axios from 'axios';
-import Register from './Register'
+import Register from './Register.vue'
 export default {
-    name:'Login'
-    ,data: () => ({
+    name:'Login',
+    components:{Register},
+    data: () => ({
       socket:null,
       valid: true,
       showError:false,
@@ -45,9 +88,7 @@ export default {
       },
       items: ['Administrator', 'Chef de parc', 'Directeur', 
       'Client','Agent de Tirage','Agent de magasin'],
-      
     }),
-    components: {Register},
     methods:{
       async login (){
          let res =(await Axios.post("http://localhost:3030/login",this.user)).data;
@@ -56,15 +97,19 @@ export default {
                 this.showError=false;
                 Axios.defaults.headers.common['Authorization']=res.token;
                 this.$store.commit('auth',res.token);
-                this.$store.dispatch('getuser');
-                this.$router.push('/dashboard');
+                this.$store.dispatch('getuser')
+                this.$router.push('/dashboard')
             }else{
                 this.showError=true;
                 this.titleError=res.title
             }
-
       }
     }
 }
 </script>
-
+<style  scoped>
+.Login-box{
+  max-width: 400px;
+  padding: 10px;
+}
+</style>
