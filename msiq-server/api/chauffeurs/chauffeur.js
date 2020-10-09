f8e020ca84bc3bd4a1e56e55dd88c3ded6bb36cc
yakoubOuldbouchiba <53263448+yakoubOuldbouchiba@users.noreply.github.com>
2020-10-09 14:34:34 +0100

    first step of authentification is done

--- msiq-server/api/chauffeurs/chauffeur.js
+++ msiq-server/api/chauffeurs/chauffeur.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
 const dboperationChauffeur = require('../../objects/chauffeurs/dboperations')
+import * as auth from '../../services/auth-service.js'
 module.exports=()=>{
     //get a list of chauffeurs
-    router.get('/chauffeurs',(req , res)=>{ 
+    router.get('/chauffeurs',auth.requireLogin,(req , res)=>{ 
         dboperationChauffeur.getChauffeurs().then(result=>{
             res.json(result[0]);
         })
     });
     //add a new chauffeur
-    router.post('/chauffeur' , (req , res)=>{
+    router.post('/chauffeur' , auth.requireLogin,(req , res)=>{
         dboperationChauffeur.setChauffeur(req.body).then(result=>{
             res.json(result);
         })
     })
     //update a chauffeur
-    router.put('/chauffeur/:id',(req , res)=>{ 
+    router.put('/chauffeur/:id',auth.requireLogin,(req , res)=>{ 
         res.send({title : 'update a chauffeur'});
     });
     // delete a chauffeur
-    router.delete('/chauffeur/:id',(req , res)=>{ 
+    router.delete('/chauffeur/:id',auth.requireLogin,(req , res)=>{ 
         dboperationChauffeur.deleteChauffeur(req.params.id).then(result=>{
             res.json(result);
         })
--- msiq-server/api/produits/produit.js
+++ msiq-server/api/produits/produit.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
 const dboperationsProduit = require('../../objects/produits/dboperations')
+import * as auth from '../../services/auth-service.js'
 module.exports=()=>{
     //get a list of produits
-    router.get('/produits',(req , res)=>{ 
+    router.get('/produits',auth.requireLogin,(req , res)=>{ 
         dboperationsProduit.getProduits().then(result=>{
             res.json(result[0]);
         })
     });
     //add a produit
-    router.post('/produit' , (req , res)=>{
+    router.post('/produit' ,auth.requireLogin ,(req , res)=>{
         dboperationsProduit.setProduit(req.body).then(result=>{
             res.json(result);
         })
     })
     //update a produit
-    router.put('/produit/:id',(req , res)=>{ 
+    router.put('/produit/:id',auth.requireLogin,(req , res)=>{ 
         res.send({title: 'update a user'});
     });
     // delete a produit
-    router.delete('/produit/:id',(req , res)=>{ 
+    router.delete('/produit/:id',auth.requireLogin,(req , res)=>{ 
         dboperationsProduit.deleteProduit(req.params.id).then(result=>{
             res.json(result);
         })
