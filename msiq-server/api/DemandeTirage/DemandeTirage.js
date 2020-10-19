c53474852a22cc6a8415d91840dc18621ed88f54
BinaryHawk <yacinelalmi19@gmail.com>
2020-10-20 01:13:42 +0200

    Fixes

--- msiq-server/api/DemandeTirage/DemandeTirage.js
+++ msiq-server/api/DemandeTirage/DemandeTirage.js
@@ -15,50 +15,52 @@ const upload = multer({
 });
 module.exports=()=>{
    //this part the get the fie 
-        router.post('/DemandeTirage', upload.single('FileData'), (req , res)=>{
-            jwt.verify((req.headers.authorization || req.headers['Authorization']),'TMPK3Y',
-            async (err,decoded) => {
-                if (err) {
-                    res.status(500).json({
+    router.post('/DemandeTirage', upload.single('FileData'), (req , res)=>{
+        jwt.verify((req.headers.authorization || req.headers['Authorization']),'TMPK3Y',
+        async (err,decoded) => {
+            if (err) {
+                res.status(500).json({
+                    title: 'Quelque chose s\'est mal passé dans le serveur',
+                    error: 'CNCTDB' 
+                })
+            };
+            console.log(req.body);
+            let infoToSend = {
+                userID: decoded.user.email,
+                OriginalFileName: req.file.originalname,
+                StoringName: req.file.filename,
+                TypeOfFile: req.file.mimetype,
+                DocTyp: req.body.DocType.substring(1,req.body.DocType.length-1),
+                AutreDes: req.body.AutreDes.substring(1,req.body.AutreDes.length-1),
+                NombreFeu: req.body.NombreFeuilles.substring(1,req.body.NombreFeuilles.length-1),
+                NombreCop: req.body.NombreCopies.substring(1,req.body.NombreCopies.length-1),
+                NombreTot: req.body.NombreTot,
+            };
+            
+            await dbOperationsDemandeTirage.setDemandeTirage(infoToSend)
+            .then(result => {
+                if(result ==='DI'){
+                    res.status(200).json({
+                        title: 'Voter demande de tirage a été envoyée'
+                    })
+                }
+                else if (result ==='CNID') {
+                    res.status(401).json({
+                        title: 'Quelque chose s\'est mal passé. Veuillez verifier vous données',
+                        error: 'CNID'
+                    })
+                } 
+                else {
+                    res.status(401).json({
                         title: 'Quelque chose s\'est mal passé dans le serveur',
                         error: 'CNCTDB' 
                     })
-                };
-                let infoToSend = {
-                    userID: decoded.user.email,
-                    OriginalFileName: req.file.originalname,
-                    StoringName: req.file.filename,
-                    TypeOfFile: req.file.mimetype,
-                    DocTyp: req.body.DocType.substring(1,req.body.DocType.length-1),
-                    AutreDes: req.body.AutreDes.substring(1,req.body.AutreDes.length-1),
-                    NombreFeu: req.body.NombreFeuilles.substring(1,req.body.NombreFeuilles.length-1),
-                    NombreCop: req.body.NombreCopies.substring(1,req.body.NombreCopies.length-1),
-                    NombreTot: req.body.NombreTotal,
-                };
-                await dbOperationsDemandeTirage.setDemandeTirage(infoToSend)
-                .then(result => {
-                    if(result ==='DI'){
-                        res.status(200).json({
-                            title: 'Voter demande de tirage a été envoyée'
-                        })
-                    }
-                    else if (result ==='CNID') {
-                        res.status(401).json({
-                            title: 'Quelque chose s\'est mal passé. Veuillez verifier vous données',
-                            error: 'CNID'
-                        })
-                    } 
-                    else {
-                        res.status(401).json({
-                            title: 'Quelque chose s\'est mal passé dans le serveur',
-                            error: 'CNCTDB' 
-                        })
-                    }
-                });
-            })
+                }
+            });
         })
-        // delete a demande
-        router.delete('/DemandeTirage/:id',(req , res)=>{
+    })
+    // delete a demande
+    router.delete('/DemandeTirage/:id',(req , res)=>{
             jwt.verify((req.headers.authorization || req.headers['Authorization']),'TMPK3Y',
             async (err,decoded) => {
                 if (err) {
@@ -87,5 +89,49 @@ module.exports=()=>{
                     })
                 });
     })
+    
+    //Get a demand
+    router.get('/DemandeTirage/:id',(req , res)=>{
+        dbOperationsDemandeTirage.GetDemandeTirage(req.params.id)
+        .then(result => {
+            if(!!result){
+                res.status(200).json({
+                    title: 'Votre demande tirage a été chargée',
+                    demande: result
+                })
+            } else {
+                res.status(401).json({
+                    title: 'Quelque chose s\'est mal passé dans le serveur',
+                    error: 'CNCTDB' 
+                })
+            }
+        })
+    });
+     
+    //update demand
+    
+    router.post('/UpdateDemandeTirage', async (req , res)=>{
+        await dbOperationsDemandeTirage.upDemandeTirage(req.body)
+            .then(result => {
+                if(result ==='DE'){
+                    res.status(200).json({
+                        title: 'Voter demande de tirage a été modifier'
+                    })
+                }
+                else if (result ==='CNED') {
+                    res.status(401).json({
+                        title: 'Quelque chose s\'est mal passé. Veuillez verifier vous données',
+                        error: 'CNED'
+                    })
+                } 
+                else {
+                    res.status(401).json({
+                        title: 'Quelque chose s\'est mal passé dans le serveur',
+                        error: 'CNCTDB' 
+                    })
+                }
+            })
+    });
+    
     return router;
 }
