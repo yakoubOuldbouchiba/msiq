a3475101459c71a751120e67c642123f482d20c2
yakoubOuldbouchiba <53263448+yakoubOuldbouchiba@users.noreply.github.com>
2020-12-29 22:29:16 +0100

    updates check it

--- src/components/Demandes/DemandeClient.vue
+++ src/components/Demandes/DemandeClient.vue
@@ -54,7 +54,7 @@
                     <v-row justify="space-around"> 
                       <v-col cols="12" sm="8"> 
                        <v-autocomplete
-                          v-model="DC.struct_id"
+                          v-model="DC.destination_id"
                           :items="destinations"
                           :readonly="!Editable"
                           item-text="description"
